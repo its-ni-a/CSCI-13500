cd52b70f6500fdcb43d6058349e2cb8989012335
Nia Hill <76218337+its-ni-a@users.noreply.github.com>
2024-03-11 19:46:28 -0400

    Update test-ascii.cpp

--- Lab_6/test-ascii.cpp
+++ Lab_6/test-ascii.cpp
@@ -1,1 +1,34 @@
+// Author: Nia Hill
+// Course: CSCI-135
+// Instructor: Tong Yi
+// Assignment: Lab 6A
+
+
+// Write a program test-ascii.cpp that asks the user to input a line of text (which may possibly include spaces).
+//  The program should report all characters from the input line together with their ASCII codes. 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string text;
+    cout << "Enter a line of text: "; 
+    
+    // getline to get inpu
+    getline(cin, text);
+    
+    for(int i = 0; i < text.size(); i++){
+        // char c = text(l);
+        cout << text[i] << " " << (int)text[i] << endl;
+    }
+    
+    return 0;
+}
+
+
+
+
+
 
